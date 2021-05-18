3259731e1d12ced678ec407a1f3d9e0dad54f770
Nifares <mateusz@serafin.sh>
2021-05-18 01:52:05 +0100

    add basic templates

--- apps/box4bet/urls.py
+++ apps/box4bet/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from .views import home, events
 
 urlpatterns = [
+    path('', home),
+    path('events', events)
 ]
--- apps/box4bet/views.py
+++ apps/box4bet/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from apps.box4bet.models import Event
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+def events(request):
+    data = {
+        'events': Event.objects.all()
+    }
+    return render(request, 'events.html', data)
