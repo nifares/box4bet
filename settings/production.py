ace0ca0d4c18f61a5ce8df6f046112206a7d8485
Mateusz <mserafin@mimecast.com>
2021-06-11 02:20:19 +0200

    production

--- settings/production.py
+++ settings/production.py
@@ -1,3 +1,12 @@
 from base import *
 
+LOGGING['loggers']['apps']['level'] = 'DEBUG'
+
 DEBUG=False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/data/db.sqlite3',
+    }
+}
